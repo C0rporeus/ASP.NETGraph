8dfc1fd5023011dfcd1d1cbff3cb610dfe556bd0
Yonathan Gutierrez <ygutierrez@cognos.com.co>
2022-03-20 20:33:35 -0500

    changes in set callendar

--- Helpers/GraphHelper.cs
+++ Helpers/GraphHelper.cs
@@ -6,11 +6,61 @@ using graphASP.NET.Models;
 using Microsoft.Graph;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+using System.Configuration;
+using System.Security.Claims;
+using graphASP.NET.TokenStorage;
+
 
 namespace graphASP.NET.Helpers
 {
     public static class GraphHelper
     {
+        // Load configuration settings from PrivateSettings.config
+        private static string appId = ConfigurationManager.AppSettings["ida:AppId"];
+        private static string appSecret = ConfigurationManager.AppSettings["ida:AppSecret"];
+        private static string redirectUri = ConfigurationManager.AppSettings["ida:RedirectUri"];
+        private static List<string> graphScopes =
+            new List<string>(ConfigurationManager.AppSettings["ida:AppScopes"].Split(' '));
+
+        public static async Task<IEnumerable<Event>> GetEventsAsync()
+        {
+            var graphClient = GetAuthenticatedClient();
+
+            var events = await graphClient.Me.Events.Request()
+                .Select("subject,organizer,start,end")
+                .OrderBy("createdDateTime DESC")
+                .GetAsync();
+
+            return events.CurrentPage;
+        }
+
+        private static GraphServiceClient GetAuthenticatedClient()
+        {
+            return new GraphServiceClient(
+                new DelegateAuthenticationProvider(
+                    async (requestMessage) =>
+                    {
+                        var idClient = ConfidentialClientApplicationBuilder.Create(appId)
+                            .WithRedirectUri(redirectUri)
+                            .WithClientSecret(appSecret)
+                            .Build();
+
+                        var tokenStore = new SessionTokenStore(idClient.UserTokenCache,
+                                HttpContext.Current, ClaimsPrincipal.Current);
+
+                        var userUniqueId = tokenStore.GetUsersUniqueId(ClaimsPrincipal.Current);
+                        var account = await idClient.GetAccountAsync(userUniqueId);
+
+                // By calling this here, the token can be refreshed
+                // if it's expired right before the Graph call is made
+                var result = await idClient.AcquireTokenSilent(graphScopes, account)
+                            .ExecuteAsync();
+
+                        requestMessage.Headers.Authorization =
+                            new AuthenticationHeaderValue("Bearer", result.AccessToken);
+                    }));
+        }
         public static async Task<CachedUser> GetUserDetailsAsync(string accessToken)
         {
             var graphClient = new GraphServiceClient(
